5227aa4216e5a2dd69ec9449e33c7553e2d5c86a
moise <moisemlg90@gmail.com>
2022-08-10 16:49:21 +0200

    Install reduxjs toolkit

--- Module_six/hello-react-front-end/src/redux/configureStore.js
+++ Module_six/hello-react-front-end/src/redux/configureStore.js
@@ -1,8 +1,8 @@
-import { combineReducer } from 'redux';
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import helloReducer from './greet';
 
-const reducer = combineReducer({
+const reducer = combineReducers({
   helloReducer,
 });
 
